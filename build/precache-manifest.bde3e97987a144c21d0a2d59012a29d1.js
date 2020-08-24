e155c686db515e88f0b455f055fd70c25e32dc6e
Rohit19060 <rohitjain19060@gmail.com>
2020-08-24 19:43:52 +0530

    testing mark1

---
+++ build/precache-manifest.bde3e97987a144c21d0a2d59012a29d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b14637a5965a117385917d894774df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e50d2808939ec35b07b",
+    "url": "/static/css/main.8933ec54.chunk.css"
+  },
+  {
+    "revision": "c62cb3ddf0c52a3d527c",
+    "url": "/static/js/2.e1d38e9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1d38e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e50d2808939ec35b07b",
+    "url": "/static/js/main.425e8105.chunk.js"
+  },
+  {
+    "revision": "2f4d27b8645c02cb3c76",
+    "url": "/static/js/runtime-main.603d9ffe.js"
+  }
+]);
