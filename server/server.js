092b6b4c474013a2e380fb1c4f02855c44c27f53
Rohit19060 <rohitjain19060@gmail.com>
2020-08-24 19:03:48 +0530

    remove yarn

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
