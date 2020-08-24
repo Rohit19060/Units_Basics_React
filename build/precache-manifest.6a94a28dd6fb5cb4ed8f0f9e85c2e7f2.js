6176df320a3d58ba32e84be1c82d8ec22e8e33b0
Rohit19060 <rohitjain19060@gmail.com>
2020-08-25 04:15:57 +0530

    test

---
+++ build/precache-manifest.6a94a28dd6fb5cb4ed8f0f9e85c2e7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3634315626cc88e26ae818becbd0ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88cca16bbe4ab4bd68e2",
+    "url": "/static/css/main.8933ec54.chunk.css"
+  },
+  {
+    "revision": "c62cb3ddf0c52a3d527c",
+    "url": "/static/js/2.e1d38e9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1d38e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88cca16bbe4ab4bd68e2",
+    "url": "/static/js/main.264b5be2.chunk.js"
+  },
+  {
+    "revision": "2f4d27b8645c02cb3c76",
+    "url": "/static/js/runtime-main.603d9ffe.js"
+  }
+]);
