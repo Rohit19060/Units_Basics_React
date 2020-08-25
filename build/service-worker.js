aca3b55066406a5387e64440275698f09acd5c30
Rohit19060 <rohitjain19060@gmail.com>
2020-08-25 07:48:25 +0530

    test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71e476eba20433dbae51d42ab29e16b1.js"
+  "/precache-manifest.50b147cc3da619ce53cba5bf7ef0993e.js"
 );
 
 self.addEventListener('message', (event) => {
